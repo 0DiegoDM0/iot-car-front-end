70c3144035648f0683d094fa8659497b12b885e6
0DiegoDM0 <cb.amp.08@gmail.com>
2024-05-23 11:46:05 -0600

    Update main.js

--- control/web/js/main.js
+++ control/web/js/main.js
@@ -10,7 +10,7 @@ function callApi(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "http://localhost/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
+      "https://18.171.167.104/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" +
         estatus
     )
     .then(function (response) {
